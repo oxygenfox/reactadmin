91729c742c06e83f62e17d17776f4650aeec6973
Your Name <andrysetyoso@gmail.comm>
2020-04-25 03:07:54 +0700

    Fix Layout Module Backup Database

--- application/modules/database/views/index.php
+++ application/modules/database/views/index.php
@@ -12,7 +12,7 @@
         <div class="card-body">
             <h3>Database Backup</h3><br>
             <!-- <a href="https://codeglamour.com/php/adminlite/admin/export/dbexport" class="btn btn-success"><i class="fa fa-download"></i> &nbsp; Download & Create Backup</a> -->
-            <form action="<?php echo base_url(); ?>database/backup" method="post">
+            <form action="<?= base_url(); ?>database/backup" method="post">
                 <div class="form-row align-items-center">
                     <div class="col-auto">
                         <label class="sr-only" for="inlineFormInput">Name</label>
@@ -25,28 +25,17 @@
                         </select>
                     </div>
                     <div class="col-auto">
-                        <button type="submit" class="btn btn-primary">Backup Database</button>
-                        <a href="<? base_url() ?>database/backup" class="btn btn-success"><i class="fa fa-download"></i> &nbsp; Backup All Database</a>
+                        <button type="submit" class="btn btn-primary"><i class="fa fa-download"></i> &nbsp;Backup Table</button>
+                        <a href="<?= base_url() ?>database/backup" class="btn btn-success"><i class="fa fa-download"></i> &nbsp; Backup All Database</a>
                     </div>
                 </div>
             </form>
             <br>
             <hr>
             <h3>Database Restore</h3><br>
-            <!-- <form action="<?php echo base_url(); ?>database/backup" method="post">
-                <select required="" name="tabel">
-                    <?php
-                    foreach ($tabel as $baris) {  ?>
-                        <option value="<?php echo $baris->Tables_in_db_cs; ?>"><?php echo $baris->Tables_in_db_cs; ?></option>
-                    <?php } ?>
-                </select>
-                <button type="submit">Backup Database</button>
-            </form> -->
-
-
             <?php echo form_open_multipart('database/restore'); ?>
             <input type="file" name="datafile" id="datafile" />
-            <button type="submit">Upload Database</button>
+            <button type="submit" class="btn btn-primary">Upload Database</button>
             </form>
         </div>
     </div>

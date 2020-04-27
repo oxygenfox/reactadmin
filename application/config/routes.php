74c1cc9007322ff717c9c869e51be820503c1662
oxygenfox <andrysetyoso@gmail.com>
2020-04-27 13:23:33 +0700

    Custom Route

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $route['default_controller'] = 'home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['panel'] = 'admin';
+$route['dashboard'] = 'admin/dashboard';
+// $route['(:any)/stats'] = 'url/stats/$1';
+// $route['(:any)'] = 'url/redirect/$1';

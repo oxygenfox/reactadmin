ded4b30c36863d81c7889f6cba7b69066dc3d796
Your Name <andrysetyoso@gmail.comm>
2020-05-01 02:45:05 +0700

    Revert cosole log

--- application/views/_layout/front/_js.php
+++ application/views/_layout/front/_js.php
@@ -1,34 +1,15 @@
 <!-- jQuery -->
-<script src="assets/plugins/jquery/jquery.min.js"></script>
+<!-- jQuery -->
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="<?= PATH_ASSETS ?>plugins/jquery-ui/jquery-ui.min.js"></script>
 <!-- Bootstrap 4 -->
 <script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
 <script src="assets/dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="assets/dist/js/demo.js"></script>
-<script src="assets/dist/vconsole.min.js"></script>
 
-<script>
-  $(document).ready(function() {
-
-    window.vConsole = new window.VConsole({
-      defaultPlugins: ['system',
-        'network',
-        'element',
-        'storage'
-      ],
-      // 可以在此设定要默认加载的面板
-      maxLogNumber: 1000,
-      // disableLogScrolling: true,
-      onReady: function() {
-        console.log('vConsole is ready.');
-      },
-      onClearLog: function() {
-        console.log('on clearLog');
-      }
-    });
-  });
-</script>
 <script src="assets/plugins/datatables/jquery.dataTables.min.js"></script>
 <script src="assets/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
 <script src="assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>

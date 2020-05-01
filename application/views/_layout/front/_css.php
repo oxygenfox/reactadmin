ca631eeb0fe5ab91b902cccaeead200e496615cc
Your Name <andrysetyoso@gmail.comm>
2020-05-01 09:16:48 +0700

    css

--- application/views/_layout/front/_css.php
+++ application/views/_layout/front/_css.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title>AdminLTE 3 | Blank Page</title>
+<title>Data Berita</title>
 <!-- Tell the browser to be responsive to screen width -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
